70837cb52fdab4bd89adcb8fb3490e3ea8a40928
Timothée Desveaux <tdesveaux@novelis.io>
2021-12-03 09:59:39 +0100

    move parts in separates functions

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -16,13 +16,21 @@ fn count_increase_windowed(depths: &Vec<i32>, window_size: usize) -> u32 {
     count
 }
 
+fn part_one(depths: &Vec<i32>) -> u32 {
+    count_increase_windowed(&depths, 1)
+}
+
+fn part_two(depths: &Vec<i32>) -> u32 {
+    count_increase_windowed(&depths, 3)
+}
+
 fn main() {
     let input = include_str!("input.txt");
 
     let depths: Vec<i32> = input.lines().map(|l| l.parse::<i32>().unwrap()).collect();
 
-    let count1 = count_increase_windowed(&depths, 1);
-    let count2 = count_increase_windowed(&depths, 3);
+    let count1 = part_one(&depths);
+    let count2 = part_two(&depths);
 
     println!(
         "There are {} measurements larger than the previous one.",
