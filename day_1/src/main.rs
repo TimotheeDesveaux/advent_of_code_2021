8a35a7e6b90fe4db67b97a003a27288179a6ddc3
Timothée Desveaux <timothee.desveaux@epita.fr>
2021-12-05 00:00:40 +0100

    add example files

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -16,21 +16,21 @@ fn count_increase_windowed(depths: &Vec<i32>, window_size: usize) -> u32 {
     count
 }
 
-fn part_one(depths: &Vec<i32>) -> u32 {
+fn part_one(input: &str) -> u32 {
+    let depths: Vec<i32> = input.lines().map(|l| l.parse::<i32>().unwrap()).collect();
     count_increase_windowed(&depths, 1)
 }
 
-fn part_two(depths: &Vec<i32>) -> u32 {
+fn part_two(input: &str) -> u32 {
+    let depths: Vec<i32> = input.lines().map(|l| l.parse::<i32>().unwrap()).collect();
     count_increase_windowed(&depths, 3)
 }
 
 fn main() {
-    let input = include_str!("input.txt");
+    let input = include_str!("example.txt");
 
-    let depths: Vec<i32> = input.lines().map(|l| l.parse::<i32>().unwrap()).collect();
-
-    let count1 = part_one(&depths);
-    let count2 = part_two(&depths);
+    let count1 = part_one(input);
+    let count2 = part_two(input);
 
     println!(
         "There are {} measurements larger than the previous one.",
