319687992f12281beca4706c9d51e937dbbabf26
Timothée Desveaux <tdesveaux@novelis.io>
2021-12-06 13:14:45 +0100

    day_6: part two solutions

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -23,10 +23,29 @@ fn part_one(input: &str) -> usize {
     fish.len()
 }
 
+fn part_two(input: &str) -> usize {
+    let mut frequencies = input
+        .split(',')
+        .map(|f| f.trim().parse::<usize>().unwrap())
+        .fold([0; 9], |mut acc, n| {
+            acc[n] += 1;
+            acc
+        });
+
+    for _ in 0..256 {
+        frequencies.rotate_left(1);
+        frequencies[6] += frequencies[8];
+    }
+
+    frequencies.iter().sum()
+}
+
 fn main() {
-    let input = include_str!("example.txt");
+    let input = include_str!("input.txt");
 
-    let number_of_fish = part_one(input);
+    let number_of_fish1 = part_one(input);
+    let number_of_fish2 = part_two(input);
 
-    println!("The number of fish after 80 days is {}", number_of_fish)
+    println!("The number of fish after 80 days is {}", number_of_fish1);
+    println!("The number of fish after 256 days is {}", number_of_fish2);
 }
