ea79ed9b77f5ac6065ad1b09cc3c5ebdd318203b
Timothée Desveaux <tdesveaux@novelis.io>
2021-12-06 10:37:23 +0100

    day_5: part two solution

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -5,42 +5,30 @@ fn register_vent(
     end: &(u32, u32),
     points_covered: &mut HashMap<(u32, u32), u32>,
 ) {
-    if start.0 == end.0 {
-        let x = start.0;
-        let mut start_y = start.1;
-        let mut end_y = end.1;
-        if start_y > end_y {
-            start_y = end.1;
-            end_y = start.1;
+    let mut cur = *start;
+
+    while cur != *end {
+        let entry = points_covered.entry(cur).or_insert(0);
+        *entry += 1;
+
+        if cur.0 < end.0 {
+            cur.0 += 1;
+        } else if cur.0 > end.0 {
+            cur.0 -= 1;
         }
 
-        for y in start_y..=end_y {
-            let entry = points_covered.entry((x, y)).or_insert(0);
-            *entry += 1;
-        }
-    } else if start.1 == end.1 {
-        let y = start.1;
-        let mut start_x = start.0;
-        let mut end_x = end.0;
-        if start_x > end_x {
-            start_x = end.0;
-            end_x = start.0;
-        }
-
-        for x in start_x..=end_x {
-            let entry = points_covered.entry((x, y)).or_insert(0);
-            *entry += 1;
+        if cur.1 < end.1 {
+            cur.1 += 1;
+        } else if cur.1 > end.1 {
+            cur.1 -= 1;
         }
     }
+
+    let entry = points_covered.entry(cur).or_insert(0);
+    *entry += 1;
 }
 
-fn find_crossing_vents(points_covered: &HashMap<(u32, u32), u32>) -> u32 {
-    points_covered
-        .iter()
-        .fold(0, |acc, (_, &count)| if count >= 2 { acc + 1 } else { acc })
-}
-
-fn part_one(input: &str) -> u32 {
+fn find_crossing_vents(input: &str, consider_diagonals: bool) -> u32 {
     let mut points_covered: HashMap<(u32, u32), u32> = HashMap::new();
 
     for line in input.lines() {
@@ -58,19 +46,33 @@ fn part_one(input: &str) -> u32 {
             end.next().unwrap().parse().unwrap(),
         );
 
-        register_vent(&start, &end, &mut points_covered);
+        if consider_diagonals || start.0 == end.0 || start.1 == end.1 {
+            register_vent(&start, &end, &mut points_covered);
+        }
     }
 
-    find_crossing_vents(&points_covered)
+    points_covered
+        .iter()
+        .fold(0, |acc, (_, &count)| if count >= 2 { acc + 1 } else { acc })
+}
+
+fn part_one(input: &str) -> u32 {
+    find_crossing_vents(input, false)
+}
+
+fn part_two(input: &str) -> u32 {
+    find_crossing_vents(input, true)
 }
 
 fn main() {
     let input = include_str!("input.txt");
 
     let count1 = part_one(input);
+    let count2 = part_two(input);
 
     println!(
         "The number of vertical and horizontal crossing vents is {}",
         count1
     );
+    println!("The number crossing vents is {}", count2);
 }
